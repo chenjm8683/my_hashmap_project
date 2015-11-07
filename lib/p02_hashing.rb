d5f7e2292d43b7bc784739b2fc038d6e4eed5e7d
Jingming Chen <chenjm8683@gmail.com>
2015-11-07 14:35:51 -0800

    finished and tested p02_hashing.rb

--- lib/p02_hashing.rb
+++ lib/p02_hashing.rb
@@ -4,15 +4,31 @@ end
 
 class Array
   def hash
+    hash_num_str = ""
+    self.to_s.each_char do |ch|
+      hash_num_str += ch.ord.to_s
+    end
+    hash_num_str.to_i
   end
 end
 
 class String
   def hash
+    # split chars in the array and store them in an arr
+    string_arr = self.split("")
+    # using the hash from array and multiply by a string-obj identifier hash
+    string_arr.hash * ["str"].hash
   end
 end
 
 class Hash
   def hash
+    # convert to sorted array of arrays that contain key and value pairs
+    hash_arr = []
+    self.keys.sort.each do |key|
+      hash_arr << [key, self[key]]
+    end
+    # using the hash from array and multiply by a hash-obj identifier hash
+    hash_arr.hash * ["hash"].hash
   end
 end
