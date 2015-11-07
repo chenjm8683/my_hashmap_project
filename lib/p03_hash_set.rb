583f613fac40c85ece030d806e246d1363b19b82
Jingming Chen <chenjm8683@gmail.com>
2015-11-07 15:55:23 -0800

    finished and tested p03_hash_set.rb

--- lib/p03_hash_set.rb
+++ lib/p03_hash_set.rb
@@ -9,18 +9,35 @@ class HashSet
   end
 
   def insert(key)
+    num = key_to_hash(key)
+    unless include?(key)
+      self[num] << key
+      @count += 1
+      resize! if @count == num_buckets
+    end
   end
 
   def include?(key)
+    num = key_to_hash(key)
+    self[num].include?(key)
   end
 
   def remove(key)
+    num = key_to_hash(key)
+    if include?(key)
+      self[num].delete(key)
+      @count -= 1
+    end
   end
 
   private
+  def key_to_hash(key)
+    key.hash
+  end
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -28,5 +45,11 @@ class HashSet
   end
 
   def resize!
+    old_store = @store
+    @store = Array.new(num_buckets * 2) { Array.new }
+    @count = 0
+    old_store.flatten.each do |el|
+      insert(el)
+    end
   end
 end
