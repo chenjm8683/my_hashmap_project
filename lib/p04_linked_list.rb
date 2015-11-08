72ad70fe258ff84423575f1b8e74f624567366f6
Jingming Chen <chenjm8683@gmail.com>
2015-11-07 16:26:06 -0800

    finished and tested p04_linked_list.rb

--- lib/p04_linked_list.rb
+++ lib/p04_linked_list.rb
@@ -11,6 +11,8 @@ class Link
 end
 
 class LinkedList
+  include Enumerable
+
   attr_reader :head
 
   def initialize
@@ -23,31 +25,86 @@ class LinkedList
   end
 
   def first
+    @head
   end
 
   def last
+    current_link = first
+    until current_link.next == nil
+      current_link = current_link.next
+    end
+    current_link
   end
 
   def empty?
+    first.key == nil
   end
 
   def get(key)
+    current_link = first
+    loop do
+      return current_link.val if current_link.key == key
+      current_link = current_link.next
+      return nil if current_link == nil
+    end
+  end
+
+  def get_link(key)
+    current_link = first
+    loop do
+      return current_link if current_link.key == key
+      current_link = current_link.next
+      return nil if current_link == nil
+    end
   end
 
   def include?(key)
+    current_link = first
+    loop do
+      return true if current_link.key == key
+      current_link = current_link.next
+      return false if current_link == nil
+    end
   end
 
   def insert(key, val)
+    if empty?
+      @head.key = key
+      @head.val = val
+    elsif include?(key)
+      get_link(key).val = val
+    else
+      last.next = Link.new(key, val)
+    end
   end
 
   def remove(key)
+    previous_link = nil
+    current_link = first
+    loop do
+      break if current_link.key == key
+      previous_link = current_link
+      current_link = current_link.next
+      return nil if current_link == nil
+    end
+    if previous_link == nil
+      @head = first.next
+    else
+      previous_link.next = current_link.next
+    end
   end
 
-  def each
+  def each(&prc)
+    current_link = first
+    loop do
+      prc.call(current_link)
+      break if current_link.next == nil
+      current_link = current_link.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
